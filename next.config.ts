1324a5c4aaa7307798f160eca34c513c15a4be96
山﨑菜穂 <yamazaki.naho@sysystem.co.jp>
2025-10-15 09:06:59 +0000

    PWAにすることはできますか？

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -33,4 +34,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { FirebaseProvider } from '@/lib/firebase.tsx';
 export const metadata: Metadata = {
   title: 'Philos',
   description: '組織改善のためのアプリ',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#f97316" />
       </head>
       <body className="font-body antialiased">
         <FirebaseProvider>
